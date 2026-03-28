fa65583f35086f7b17260553c7acf8b765a8618e
ImWinchester <iortiz1823@bc.conalep.edu.mx>
2026-03-28 01:23:52 -0700

    Change login username from 'profe' to 'Hugo'

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
   let user = document.getElementById("user").value;
   let pass = document.getElementById("pass").value;
  
-  if(user === "profe" && pass === "1234") {
+  if(user === "Hugo" && pass === "1234") {
     window.location.href = "dashboard.html";
   } else {
     document.getElementById("error").innerText = "Datos incorrectos";
